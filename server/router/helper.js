c787cf0e7227028b3ffe8fb3655770f3422202ec
Facundo <facugon@theeye.io>
2020-08-04 13:52:00 -0300

    validate empty and req. errors (#39)

--- server/router/helper.js
+++ server/router/helper.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const got = require('got')
+const { ClientError } = require('../errors')
 
 module.exports = (app) => {
   const router = express.Router()
@@ -10,13 +11,26 @@ module.exports = (app) => {
    */
   router.get('/script/example/:extension', async (req, res, next) => {
     try {
+      let extension = req.params.extension
+
+      if (!extension || extension === "null") {
+        throw new ClientError('Extension required')
+      }
+
       const scriptsPath = 'https://raw.githubusercontent.com/theeye-io/theeye-docs/master/scripts/examples/'
-      const url = scriptsPath + 'example.' + req.params.extension
+      const url = scriptsPath + 'example.' + extension
 
       let response = await got(url, {})
       res.status(response.statusCode).send(response.body)
     } catch (err) {
-      next(err)
+      // handle GOT errors
+      if (err.name === 'HTTPError') {
+        if (err.message.includes('Response code 404')) {
+          next(new ClientError('Not Found'))
+        }
+      } else {
+        next(err)
+      }
     }
   })
 
