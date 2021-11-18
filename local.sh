1e7a77ff239329a6964ff95500bebc48b415a32f
facugon <facugon@theeye.io>
2021-11-18 18:11:51 -0300

    fix: Added supervisor public url (#1)
    Merged changes from theeye-io-team/supervisor-url

--- local.sh
+++ local.sh
@@ -2,9 +2,13 @@
 
 #NODE_ENV=comefa DEBUG=*eye* npx nodemon --ignore ./client ${1} server/
 if [ -z "${DEBUG}" ]; then
-  export DEBUG=*eye*
+  export DEBUG="*eye*"
 fi
 
-NODE_ENV=dev npx nodemon --ignore ./client ${1} server/
+if [ -z "${NODE_ENV}" ]; then
+  export NODE_ENV="dev"
+fi
+
+npx nodemon --ignore ./client ${1} server/
 
 #NODE_ENV=dev DEBUG=*theeye:log:router* npx nodemon ${1} server/
