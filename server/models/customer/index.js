073c3e4eeb138eac5a9ac02c613016ee3dc28ca9
Ezequiel Korenblum <ezequiel@losko.com.ar>
2022-03-04 12:48:15 -0300

    enterprise login

--- server/models/customer/index.js
+++ server/models/customer/index.js
@@ -27,7 +27,8 @@ module.exports = function (db) {
             authtoken: '',
             address: '',
             protocol: ''
-          }
+          },
+          enterprise_login: null
         }
       }
     },
