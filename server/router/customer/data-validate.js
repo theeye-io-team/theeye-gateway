1bb2a3d07de4916b1a2b9f98ccc640ca1615b2a6
facugon <facugon@theeye.io>
2022-03-14 17:00:02 -0300

    clarification added to message

--- server/router/customer/data-validate.js
+++ server/router/customer/data-validate.js
@@ -5,7 +5,7 @@ const { validUsername } = require('../user/data-validate')
 
 const validateCustomerName = async (app, name) => {
   if (!validUsername(name)) {
-    throw new ClientError('The organization name can contains 6 to 20 letters (a-z), numbers (0-9), period (.), underscore (_) and hyphen (-)')
+    throw new ClientError('The organization name can contains 6 to 20 letters (a-z), numbers (0-9), period (.), underscore (_) and hyphen (-). It must starts and ends with an alphanumeric symbol')
   }
 
   customer = await app.models.customer.findOne({ name: new EscapedRegExp(name, 'i') })
