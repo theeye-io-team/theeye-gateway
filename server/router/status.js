82e587fc1fd071c05ba57a796edab36c64868354
facugon <facugon@theeye.io>
2023-04-11 14:57:00 -0300

    feat: remote options test api

--- server/router/status.js
+++ server/router/status.js
@@ -8,6 +8,33 @@ module.exports = (app) => {
 
   router.get('/', nodeStatus)
 
+  router.get('/data', (req, res, next) => {
+    const data = [
+      { id: 1, value:  1 },
+      { id: 2, value:  2 },
+      { id: 3, value:  3 },
+      { id: 4, value:  4 },
+      { id: 5, value:  5 },
+      { id: 6, value:  6 },
+      { id: 7, value:  7 },
+      { id: 8, value:  8 },
+      { id: 9, value:  9 },
+      { id: 10, value: 10 },
+      { id: 11, value: 11 },
+      { id: 12, value: 12 },
+      { id: 13, value: 13 },
+      { id: 15, value: 15 },
+      { id: 16, value: 16 },
+    ]
+
+    if (req.query.q) {
+      res.json(
+        data.filter(d => new RegExp(req.query.q).test(d.value) === true)
+      )
+    }
+    res.json(data)
+  })
+
   return router
 }
 
