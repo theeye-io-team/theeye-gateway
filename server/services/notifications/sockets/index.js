5e7c4bbdf4b502bc2d66ca1059efa812b1b37509
facugon <facugon@theeye.io>
2023-07-11 10:39:07 -0300

    deprecated: JOB_RESULT_RENDER removed

--- server/services/notifications/sockets/index.js
+++ server/services/notifications/sockets/index.js
@@ -94,7 +94,6 @@ module.exports = function (app, config) {
 
       // user session subscriptions
       joinRoom(socket, `${customer_id}:${user_id}:${TopicConstants.NOTIFICATION_CRUD}`)
-      joinRoom(socket, `${customer_id}:${user_id}:${TopicConstants.JOB_RESULT_RENDER}`)
       joinRoom(socket, `${session_id}:${user_id}:${TopicConstants.SESSION}`)
 
       if (app.service.authentication.acl.hasFullaccess(session.credential)) {
